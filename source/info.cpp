4070afbb538aa84ec62ce4f950488e8837135c1e
Parvilus Love Marckendy <lovemarckendyparvilus@gmail.com>
2020-08-03 19:42:30 -0400

    7eme sauvegarde

--- source/info.cpp
+++ source/info.cpp
@@ -1,10 +1,92 @@
 #include "info.h"
 
-Info::Info(sf::RenderWindow& fenetre)
+Info::Info(sf::RenderWindow& fenetre):m_fenetre(0)
+{
+    m_fenetre=&fenetre;
+    m_score=0;
+    m_niveau=1;
+    m_nombrLignes=0;
+
+
+
+}
+
+void Info::initInfo()
+{
+    //les fonts
+    m_fntChiffres.loadFromFile("donnees/keypunc.ttf");
+    m_fntNoms.loadFromFile("donnees/ALGER.ttf");
+
+    //les textes
+    m_txtNiveau.setFont(m_fntChiffres);
+    m_txtNiveau.setCharacterSize(30);
+    m_txtNiveau.setPosition(19.2f*20.0f,17.1f*20.f);
+    m_txtNiveau.setFillColor(sf::Color::Black);
+
+
+    m_txtNomsJ.setFont(m_fntNoms);
+    m_txtNomsJ.setCharacterSize(30);
+    m_txtNomsJ.setPosition(18.3f*20.0f,21.f*20.f);
+    m_txtNomsJ.setFillColor(sf::Color::Black);
+
+
+    m_txtScoresJ.setFont(m_fntNoms);
+    m_txtScoresJ.setCharacterSize(30);
+    m_txtScoresJ.setPosition(22.f*20.0f,21.f*20.f);
+    m_txtScoresJ.setFillColor(sf::Color::Black);
+
+
+
+    m_txtInfoJeu.setFont(m_fntNoms);
+    m_txtInfoJeu.setCharacterSize(30);
+    m_txtInfoJeu.setPosition(3.9f*20.0f,20.7f*20.f);
+    m_txtInfoJeu.setFillColor(sf::Color::Black);
+}
+
+void Info::reinitInfo()
+{
+    m_score=0;
+    m_niveau=1;
+    m_nombrLignes=0;
+}
+
+void Info::chargementDonnees()
+{
+    FILE * fichierNoms=NULL;
+
+}
+
+void Info::gestTableauScore()
 {
 
 }
 
+void Info::gestInfoPartEnCours(int action)
+{
+
+}
+
+void Info::gestMeilleurJ()
+{
+
+}
+
+void Info::maj_Info()
+{
+
+}
+
+void Info::afficheInfo()
+{
+
+}
+
+template <class T> std::string Info::nbEnString(T nb)
+{
+    std::ostringstream chaineCar;
+    chaineCar << nb;
+    return chaineCar.str();
+}
 
 Info::~Info()
 {
--- source/info.h
+++ source/info.h
@@ -5,6 +5,7 @@
 #include "enums.h"
 #include "varG.h"
 #include "constantes.h"
+#include "sstream"
 
 class Info
 {
@@ -12,16 +13,19 @@ public:
     Info(sf::RenderWindow& fenetre);
     void initInfo();
     void reinitInfo();
+    void chargementDonnees();
     void gestTableauScore();
     void gestInfoPartEnCours(int action);
     void gestMeilleurJ();
+    void maj_Info();
     void afficheInfo();
+
+    template <class T> std::string nbEnString(T nb);
     ~Info();
 
 
 private:
     sf::RenderWindow *m_fenetre;
-
     sf::Text m_txtNiveau;
     sf::Text m_txtNomsJ;
     sf::Text m_txtScoresJ;
@@ -29,6 +33,8 @@ private:
 
     sf::Font m_fntNoms;
     sf::Font m_fntChiffres;
+
+    std::ostringstream m_streamNomsJ,m_streamScoresJ,m_streamInfoJeu;
 
     int m_score;
     int m_niveau;
